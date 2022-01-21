c792ef9e655582ed0a67871f7829389da54009d3
saradelrio131 <97974312+saradelrio131@users.noreply.github.com>
2022-01-21 01:29:53 +0100

    Update test_put_todo_error

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -80,7 +80,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         from src.todoList import put_item
         # Table mock
         self.assertRaises(Exception, put_item("", self.dynamodb))
-        self.assertRaises(Exception, put_item("", self.dynamodb))
+        self.assertRaises(ClientError, put_item("", self.dynamodb))
         print ('End: test_put_todo_error')
 
     def test_get_todo(self):
