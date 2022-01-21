5b1fae205528f089851df0c20bd79d4dde98b72c
saradelrio131 <97974312+saradelrio131@users.noreply.github.com>
2022-01-21 12:44:09 +0100

    Update test_put_todo_error

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -81,11 +81,12 @@ class TestDatabaseFunctions(unittest.TestCase):
         # Testing file functions
         from src.todoList import put_item
         # Table mock
-        self.assertRaises(Exception, put_item("", self.dynamodb))         
-        try:
-            put_item(1, "")
-        except ClientError:
-            pytest.fail("Unexpected MyError ..")
+        # self.assertRaises(Exception, put_item("", self.dynamodb)) 
+        
+        with pytest.raises(ClientError, match='Invalid input') as e:
+            assert put_item(1, "")
+              
+      
         print ('End: test_put_todo_error')
 
     def test_get_todo(self):
